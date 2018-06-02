46e049453003fa8bd655b82d4afece968f200b76
Chris Hall <followingthepath@gmail.com>
2018-06-02 18:31:30 +1000

    Adding generic Lexer

---
+++ src/Shared/Lexer.hs
@@ -0,0 +1,91 @@
+module Shared.Lexer
+(
+    Token (..),
+    lexer,
+    stringify_token,
+    stringify_tokens,
+)
+where
+
+import Control.Applicative
+import Data.Char (isSpace, isAlphaNum)
+
+data Token = Lambda
+           | Period
+           | LParen
+           | RParen
+           | Symbol String
+    deriving (Show, Eq)
+
+stringify_token :: Token -> String
+stringify_token Lambda = "\\"
+stringify_token Period = "."
+stringify_token LParen = "("
+stringify_token RParen = ")"
+stringify_token (Symbol str) = str
+
+stringify_tokens :: [Token] -> String
+stringify_tokens [] = ""
+stringify_tokens ((Symbol s1):s2@(Symbol _):rest) = (s1) ++ " " ++ stringify_tokens (s2:rest)
+stringify_tokens (first:rest) = (stringify_token first) ++  (stringify_tokens rest)
+
+lexer :: String -> Either String [Token]
+lexer ""                     = Right []
+lexer (ch:rest) | isSpace ch = lexer (trim_space rest)
+lexer string                 =
+    case     lexer_lambda string
+         <|> lexer_period string
+         <|> lexer_lparen string
+         <|> lexer_rparen string
+         <|> lexer_symbol string of
+             Nothing                  -> Left $ "Failed to parse: " ++ string
+             Just (token, [])         -> Right [token]
+             Just (ltoken, remaining) -> case lexer remaining of
+                                               Left str -> Left str
+                                               Right rtokens -> Right (ltoken:rtokens)
+
+trim_space :: String -> String
+trim_space (ch:rest) | isSpace ch = trim_space rest
+trim_space str = str
+
+lexer_lambda :: String -> Maybe (Token, String)
+lexer_lambda ('\\':rest) = Just (Lambda, rest)
+lexer_lambda _           = Nothing
+
+lexer_period :: String -> Maybe(Token, String)
+lexer_period ('.':rest) = Just (Period, rest)
+lexer_period _          = Nothing
+
+lexer_lparen :: String -> Maybe(Token, String)
+lexer_lparen ('(':rest) = Just (LParen, rest)
+lexer_lparen _          = Nothing
+
+lexer_rparen :: String -> Maybe(Token, String)
+lexer_rparen (')':rest) = Just (RParen, rest)
+lexer_rparen _          = Nothing
+
+lexer_symbol :: String -> Maybe (Token, String)
+lexer_symbol string = lexer_symbol' string ""
+
+--             input     symbol so far
+lexer_symbol' :: String -> String -> Maybe (Token, String)
+lexer_symbol'   (ch:rest)  ""     | should_discard ch = lexer_symbol' rest ""
+lexer_symbol'   (ch:rest)  ""     | isAlphaNum ch     = lexer_symbol' rest [ch]
+lexer_symbol'   (ch:rest)  ""                         = Nothing
+lexer_symbol'   (ch:rest)  sym    | should_break ch   = Just (Symbol sym, ch:rest)
+lexer_symbol'   (ch:rest)  sym    | isAlphaNum ch     = lexer_symbol' rest (sym++[ch])
+lexer_symbol'   (ch:rest)  sym                        = Just (Symbol sym, ch:rest)
+lexer_symbol'   []         sym                        = Just (Symbol sym, [])
+
+should_discard :: Char -> Bool
+should_discard ch | isSpace ch = True
+should_discard _               = False
+
+should_break :: Char -> Bool
+should_break ch | isSpace ch = True
+should_break '('             = True
+should_break ')'             = True
+should_break '.'             = True
+should_break '\\'            = True
+should_break _               = False
+
---
+++ test/Shared/LexerSpec.hs
@@ -0,0 +1,84 @@
+module Shared.LexerSpec
+(
+    main,
+    spec,
+)
+where
+
+import Test.Hspec
+
+import Shared.Lexer
+
+spec :: Spec
+spec = do
+  describe "stringify tests" $ do
+    it "stringify_tokens 1" $ do
+      let expression = [LParen, Lambda, Symbol "x", Period, Symbol "x", RParen]
+      let expected = "(\\x.x)"
+      stringify_tokens expression `shouldBe` expected
+    it "stringify_tokens 2" $ do
+      let expression = [LParen, Lambda, Symbol "x", Period, Symbol "x", Symbol "x", Symbol "x", RParen]
+      let expected = "(\\x.x x x)"
+      stringify_tokens expression `shouldBe` expected
+
+  describe "single lexer tests" $ do
+    it "lambda" $ do
+      let expression = "\\"
+      let expected = Right [Lambda]
+      lexer expression `shouldBe` expected
+    it "period" $ do
+      let expression = "."
+      let expected = Right [Period]
+      lexer expression `shouldBe` expected
+    it "parens" $ do
+      let expression = "()"
+      let expected = Right [LParen, RParen]
+      lexer expression `shouldBe` expected
+    it "symbol 1" $ do
+      let expression = "x"
+      let expected = Right [Symbol "x"]
+      lexer expression `shouldBe` expected
+    it "symbol 2" $ do
+      let expression = "hello"
+      let expected = Right [Symbol "hello"]
+      lexer expression `shouldBe` expected
+    it "symbol 3" $ do
+      let expression = "     hello     world   "
+      let expected = Right [Symbol "hello", Symbol "world"]
+      lexer expression `shouldBe` expected
+    it "symbol 4" $ do
+      let expression = "abc123"
+      let expected = Right [Symbol "abc123"]
+      lexer expression `shouldBe` expected
+    it "all spaces" $ do
+      let expression = "     "
+      let expected = Right []
+      lexer expression `shouldBe` expected
+
+  describe "compound lexer tests" $ do
+    it "simple lambda" $ do
+      let expression = "(\\x.x)"
+      let expected = Right [LParen, Lambda, Symbol "x", Period, Symbol "x", RParen]
+      lexer expression `shouldBe` expected
+    it "fixpoint" $ do
+      let expression = "(\\x.x x x)"
+      let expected = Right [LParen, Lambda, Symbol "x", Period, Symbol "x", Symbol "x", Symbol "x", RParen]
+      lexer expression `shouldBe` expected
+
+  describe "lexer error handling" $ do
+    it "unknown char" $ do
+      let expression = "["
+      let expected = Left "Failed to parse: ["
+      lexer expression `shouldBe` expected
+    it "spaces" $ do
+      let expression = "  ["
+      let expected = Left "Failed to parse: ["
+      lexer expression `shouldBe` expected
+    it "invalid symbol" $ do
+      let expression = "he]"
+      let expected = Left "Failed to parse: ]"
+      lexer expression `shouldBe` expected
+
+main :: IO ()
+main = hspec spec
+
